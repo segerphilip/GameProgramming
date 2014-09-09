84216d7478c9a0168b6bdff59d3379c8e22c3c94
Philip Seger <seger.philip@gmail.com>
2014-09-08 23:13:20 -0400

    moving up implemented

--- Assignments/2048_Implementation/2048.py
+++ Assignments/2048_Implementation/2048.py
@@ -1,38 +1,77 @@
+import random
+
 def start():
 	print '--------------------------------\n            2048\n--------------------------------'
-	board()
-	move()
+	grid = []
+	board(grid)
+	move(grid)
 
-def move():
+def move(grid):
 	move = raw_input('Left, right, up, or down? Use the arrow keys! ')
 	if move == '\x1b[A':
-		print 'up'
-		boardMove('up')
+		boardMove('up', grid)
 	elif move == '\x1b[B':
-		print 'down'
-		boardMove('down')
+		boardMove('down', grid)
 	elif move == '\x1b[C':
-		print 'right'
-		boardMove('right')
+		boardMove('right', grid)
 	elif move == '\x1b[D':
-		print 'left'
-		boardMove('left')
+		boardMove('left', grid)
+	elif move == 'q':
+		return
 	else:
 		print "That's not a valid move"
 		return move()
 
-def boardMove(direction):
-	return
+def boardMove(direction, grid):
+	print direction
+	if direction == 'up':
+		i = 0
+		for j in range(0,4):
+			if grid[i][j]!=0 or grid[i+1][j]!=0 or grid[i+2][j]!=0 or grid[i+3][j]!=0:
+				if grid[i][j]==0:
+					while grid[i][j]==0:
+						grid[i][j]=grid[i+1][j]
+						grid[i+1][j]=grid[i+2][j]
+						grid[i+2][j]=grid[i+3][j]
+						grid[i+3][j]=0
+				if grid[i+1][j]==0 and (grid[i+2][j]!=0 or grid[i+3][j]!=0):
+					while grid[i+2][j]==0:
+						grid[i+2][j]=grid[i+3][j]
+						grid[i+3][j]=0
 
-def board():
-	grid = []
+
+
+
+
+
+	if direction == 'left':
+		for row in range(3):
+			for column in range(3):
+				if grid[row][column]!=0 or grid[row][column+1]!=0 or grid[row][column+2]!=0 or grid[row][column+3]!=0:
+					grid[row
+				elif grid[row][column+1] == grid[row][column]:
+					grid[row][column] = grid[row][column]*2
+					grid[row][column+1] = 0
+				else:
+					grid[row][column] = grid[row][column+1]
+	printGrid(grid)
+	return move(grid)
+
+def board(grid):
 	for row in range(4):
 		grid.append([])
 		for column in range(4):
 			grid[row].append(0)
+	x = random.randint(0,3)
+	y = random.randint(0,3)
+	grid[x][y] = 2
+	grid[y][x] = 2
+	printGrid(grid)
+	return grid
+
+def printGrid(grid):
 	for row in range(4):
 		print grid[row]
-	return grid
 
 if __name__ == '__main__':
 	start()
